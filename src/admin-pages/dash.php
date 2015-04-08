b37a0a442ee6ee27e39c87b7ba2ce8b4f30d05d1
Marc Godard <me@marcgodard.com>
2015-04-07 21:39:23 -0400

    Started Dash Console
    and file scanning.

--- src/admin-pages/dash.php
+++ src/admin-pages/dash.php
@@ -2,12 +2,65 @@
 
 include_once('header.php');
 
+$files = scanFiles('.html');
+
 ?>
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">Dashboard</h1>
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
 
-
-
-
-
+    <div class="row">
+        <div class="col-lg-4">
+            <div class="panel panel-red">
+                <div class="panel-heading">
+                    Unprocessed Files
+                </div>
+                <div class="panel-body">
+                    <ul>
+                        <?php foreach($files as $file) { ?>
+                            <li><?=$file?></li>
+                        <?php } ?>
+                    </ul>
+                </div>
+                <div class="panel-footer">
+                    <?=count($files)?> Listed Files
+                </div>
+            </div>
+            <!-- /.col-lg-4 -->
+        </div>
+        <div class="col-lg-4">
+            <div class="panel panel-yellow">
+                <div class="panel-heading">
+                    Files Missing Content Management
+                </div>
+                <div class="panel-body">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt est vitae ultrices accumsan. Aliquam ornare lacus adipiscing, posuere lectus et, fringilla augue.</p>
+                </div>
+                <div class="panel-footer">
+                    Panel Footer
+                </div>
+            </div>
+            <!-- /.col-lg-4 -->
+        </div>
+        <div class="col-lg-4">
+            <div class="panel panel-green">
+                <div class="panel-heading">
+                    Completed Files
+                </div>
+                <div class="panel-body">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt est vitae ultrices accumsan. Aliquam ornare lacus adipiscing, posuere lectus et, fringilla augue.</p>
+                </div>
+                <div class="panel-footer">
+                    Panel Footer
+                </div>
+            </div>
+            <!-- /.col-lg-4 -->
+        </div>
+    </div>
+    <!-- /.row -->
 <?php
 
 include_once('footer.php');
--- src/system/functions.php
+++ src/system/functions.php
@@ -1,5 +1,14 @@
 <?php
 
+function scanFiles($endsWith) {
+    $files = scandir('../');
+    $arr = Array();
+    foreach ($files as $file) {
+        if (endsWith($file, $endsWith)) $arr[] = $file;
+    }
+    return $arr;
+}
+
 function authNeeded() {
     $json = json_decode(file_get_contents("data/access.json"), true);
     return sizeof($json) === 0;
@@ -29,6 +38,12 @@ function search($array, $key, $value) {
             $results = array_merge($results, search($subArray, $key, $value));
         }
     }
-
     return $results;
+}
+
+function endsWith ($string, $test) {
+    $strLen = strlen($string);
+    $testLen = strlen($test);
+    if ($testLen > $strLen) return false;
+    return substr_compare($string, $test, $strLen - $testLen, $testLen) === 0;
 }
